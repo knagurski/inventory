80c3a02f01e9a4eb14ad1e8778a7e383b2196da2
Kevin Nagurski <knagurski@gmail.com>
2017-04-13 10:45:29 +0100

    Add some basic e2e tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,18 +2,65 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function (browser) {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
+  'Page loads with no items': browser => {
     const devServer = browser.globals.devServerURL
 
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.listing--no-items')
+      .end()
+  },
+
+  'Shows load fixtures dialog on load with no items': browser => {
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.listing--no-items .dialog')
+      .assert.containsText('.listing--no-items .dialog', 'Would you like me to load')
+      .end()
+  },
+
+  'Loads fixtures': browser => {
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.listing--no-items .dialog')
+      .assert.elementPresent('.listing--no-items .dialog button')
+      .click('.listing--no-items .dialog button')
+      .waitForElementVisible('.listing__items', 5000)
+      .assert.elementCount('.item-card', 6)
+      .end()
+  },
+
+  'Triggers form when item clicked': browser => {
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .click('.listing--no-items .dialog button')
+      .waitForElementVisible('.listing__items', 5000)
+      .click('.item-card:first-child')
+      .waitForElementVisible('.item-form', 5000)
+      .assert.elementPresent('.item-form--show')
+      .end()
+  },
+
+  'Triggers form when "Add" button pressed': browser => {
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.sidebar .button__add')
+      .click('.sidebar .button__add')
+      .waitForElementVisible('.item-form', 5000)
+      .assert.elementPresent('.item-form--show')
       .end()
   }
 }
